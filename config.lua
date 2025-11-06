5b01ca9b2611533b5c05ed9b16b2d07a56ca2062
Viewmatrix <66967403+Alleexxi@users.noreply.github.com>
2025-11-06 14:46:28 +0100

    Clarify AggressiveDetection comment in config.lua
    Updated comment for AggressiveDetection to clarify its impact.

--- config.lua
+++ config.lua
@@ -5,4 +5,4 @@ Config = {}
 -- ban = Bans the player    TODO
 -- kick = kick              TODO
 Config.OnDetection = "AddToValid" -- ban or AddToValid
-Config.AggressiveDetection = false -- If you enable this, you need to get the valid patterns yourself. For example using AddToValid
+Config.AggressiveDetection = false -- If you enable this, patterns might be invalid if you change something in your code
